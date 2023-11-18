677dd956ee3132a835ae3ddb4de48869dd1b94cb
lgustavobarre351 <rm99210@fiap.com>
2023-11-18 09:40:24 -0300

    commit terminando rotas

--- global-site/src/components/Cabecalho/Cabecalho.js
+++ global-site/src/components/Cabecalho/Cabecalho.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import Logo from './logo.png'; 
-import { Link } from 'react-router-dom';
+import React from "react";
+import Logo from "./logo.png";
+import { Link } from "react-router-dom";
 
 function Cabecalho() {
   return (
-   <div className='Cabecalho'>
-    <div className='Marca_logo'>
-        <img className='Marca' src={Logo} alt="Logo" />
-        <h1 className='HealthGuardian'>Health Guardian</h1>  
+    <div className="Cabecalho">
+      <div className="Marca_logo">
+        <img className="Marca" src={Logo} alt="Logo" />
+        <h1 className="HealthGuardian">Health Guardian</h1>
+      </div>
+      <div className="Links">
+        <Link to="/HealthGuardian">HealthGuardian</Link>
+        <Link to="/Login">Cria</Link>
+      </div>
+      <Link className="Deslogar" to="/Login">
+        Deslogar
+      </Link>
     </div>
-      <div className='Links'>
-      <Link to="/HealthGuardian">HealthGuardian</Link>  
-      <Link to="/Login">Cria</Link>  
-      </div>  
-      <Link className='Deslogar' to="/Login">Deslogar</Link>  
-   </div>
   );
 }
 
